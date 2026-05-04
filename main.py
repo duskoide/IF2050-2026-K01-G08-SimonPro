d6ba473224a7fdebfab3ad34131e046efa1e3e5c
Benedicta Sherin <benedictasherin@gmail.com>
2026-05-04 22:34:06 +0700

    style: set Inter as default application font

--- main.py
+++ main.py
@@ -11,6 +11,20 @@ def main() -> int:
     """Run the SiMonPro application."""
     app = QApplication(sys.argv)
 
+    import os
+    from PyQt6.QtGui import QFontDatabase, QFont
+
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    font_path = os.path.join(base_dir, "fonts", "Inter-Regular.ttf")
+
+    font_id = QFontDatabase.addApplicationFont(font_path)
+
+    if font_id != -1:
+        families = QFontDatabase.applicationFontFamilies(font_id)
+        app.setFont(QFont(families[0], 10))
+    else:
+        print("Font gagal load!")
+
     window = MainWindow()
     window.show()
 
